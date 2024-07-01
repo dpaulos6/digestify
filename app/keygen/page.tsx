89a0ac6a19287aec3ef0ddc238d6c0f025149a5a
dpaulos6 <itzframepvp@outlook.com>
2024-07-01 16:07:19 +0100

    Secret generation now updates on page mount

--- app/keygen/page.tsx
+++ app/keygen/page.tsx
@@ -1,16 +1,27 @@
-import CopyToClipboardWrapper from '@/components/copy-to-clipboard'
-import { generateRandomSecret } from '@/helpers/crypto'
-import { Metadata } from 'next'
+'use client'
 
-export const metadata: Metadata = {
-  title: 'My Next Page'
-}
+import { useEffect, useState } from 'react'
+import CopyToClipboardWrapper from '@/components/copy-to-clipboard'
+import { Metadata } from 'next'
+import { generateRandomSecret } from '@/helpers/crypto'
+
+// export const metadata: Metadata = {
+//   title: 'Digestify - Key Generation'
+// }
 
 export default function SecretGenerator() {
+  const [secret32, setSecret32] = useState('')
+  const [secret64, setSecret64] = useState('')
+
+  useEffect(() => {
+    setSecret32(generateRandomSecret(32))
+    setSecret64(generateRandomSecret(64))
+  }, [])
+
   return (
     <main className="flex-1 flex flex-col items-center">
       <span className="flex items-center gap-2 text-3xl mt-12">
-        Your random generated Secrets.
+        Your randomly generated Secrets.
       </span>
       <section className="flex flex-col max-w-5xl items-start justify-center w-full gap-4 my-12 px-12">
         <div className="flex gap-2 items-center">
@@ -18,7 +29,7 @@ export default function SecretGenerator() {
             Secret 32 characters
           </label>
           <CopyToClipboardWrapper position="outside">
-            {generateRandomSecret(32)}
+            {secret32}
           </CopyToClipboardWrapper>
         </div>
         <div className="flex gap-2 items-center">
@@ -26,7 +37,7 @@ export default function SecretGenerator() {
             Secret 64 characters
           </label>
           <CopyToClipboardWrapper position="outside">
-            {generateRandomSecret(64)}
+            {secret64}
           </CopyToClipboardWrapper>
         </div>
       </section>
