4e073434c4e4d946add43a7273d8fb9022b21d78
Hugo <gitjohan5@gmail.com>
2024-07-27 10:15:08 +0300

    createDecipheriv error fix

--- helpers/encryption/encryption.ts
+++ helpers/encryption/encryption.ts
@@ -13,18 +13,16 @@ export function encryptTextAES(text: string): string {
   )
   let encrypted = cipher.update(text, 'utf8', 'hex')
   encrypted += cipher.final('hex')
-  // Concatenate IV and encrypted data with a delimiter (e.g., ':')
   return `${AES_IV.toString('hex')}:${encrypted}`
 }
 
 // AES Decryption
 export function decryptTextAES(encryptedText: string): string {
-  // Split the input string to get the IV and encrypted data
   const [iv, encryptedData] = encryptedText.split(':')
   const decipher = crypto.createDecipheriv(
     'aes-256-cbc',
     Uint8Array.from(AES_KEY),
-    Buffer.from(iv, 'hex')
+    Uint8Array.from(Buffer.from(iv, 'hex'))
   )
   let decrypted = decipher.update(encryptedData, 'hex', 'utf8')
   decrypted += decipher.final('utf8')
